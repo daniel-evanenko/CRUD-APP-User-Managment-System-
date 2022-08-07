37d325b083804ac16d753f8c423e576f5f2cbb63
daniel <daniel.evanenko@gmail.com>
2022-08-07 14:30:04 +0300

    using api to show users from database

--- server/services/render.js
+++ server/services/render.js
@@ -1,6 +1,16 @@
+const { default: axios } = require('axios');
+const exios = require('axios');
+
+
+
 
 exports.homeRoutes = (req,res) => {
-    res.render('index');
+    axios.get('http://localhost:3000/api/users')
+    .then(function(response){ 
+        res.render('index',{users:response.data});
+    }).catch(err=>{
+        res.send(err);
+    })
 
 }
 
