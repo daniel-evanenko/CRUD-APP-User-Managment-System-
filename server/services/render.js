18da092ef6ab803b3738914341614c075abd931a
daniel <daniel.evanenko@gmail.com>
2022-08-07 17:41:39 +0300

    update user

--- server/services/render.js
+++ server/services/render.js
@@ -14,10 +14,16 @@ exports.homeRoutes = (req, res) => {
     
 }
 
-exports.add_user=(req,res) =>{
+exports.add_user = (req, res) =>{
     res.render('add_user');
 }
 
-exports.update_user = (req,res) => {
-    res.render('update_user');
+exports.update_user = (req, res) =>{
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+        .then(function(userdata){
+            res.render("update_user", { user : userdata.data})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
 }
